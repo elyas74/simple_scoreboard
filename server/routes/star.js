c5458f9860e04f6147723237f0de14402500f6f7
elyas ghasemi <egh1374@gmail.com>
2015-06-04 13:52:20 +0430

    add req per IP counter to prevent simple DDoS attacks

--- server/routes/star.js
+++ server/routes/star.js
@@ -7,32 +7,55 @@ var include_guest_get = [ '/' ],
     exclude_ajax_post = [ '/' ];
 
 
+var black_list = {};
+var printed_black_list = {} ;
+
+setInterval(function(){
+    black_list = {};
+    printed_black_list = {};
+    // console.log("clean the IPs");
+},60000);
+
+
 module.exports = (function () {
     var _return = {};
 
     _return.get = function (req, res, next) {
 
-//        console.log(req.url);
+        black_list[req.ip] = black_list[req.ip] + 1 || 1 ;
 
-        /*if (req.url == '/del' && !req.session.q) {
+        // console.open(black_list);
+        if( black_list[req.ip] < global.init.req_per_min){
             next();
-        }
-        else */if (req.url == '/del') {
-            next();
-        } else if (req.session.q) {
-            db.contacts.findOne({_id: req.session.q }).lean().exec(
-                function (err, contact) {
-                    if (contact) {
-                            req.contact = contact;
-                    } else {
-                        res.render('auth_error');
-                    }
-                });
-        } else if (_.contains(include_guest_get, req.url)) {
-            next();
+    //        console.log(req.url);
+
+            /*if (req.url == '/del' && !req.session.q) {
+                next();
+            }
+            else */ /*if (req.url == '/del') {
+                next();
+            } else if (req.session.q) {
+                db.contacts.findOne({_id: req.session.q }).lean().exec(
+                    function (err, contact) {
+                        if (contact) {
+                                req.contact = contact;
+                        } else {
+                            res.render('auth_error');
+                        }
+                    });
+            } else if (_.contains(include_guest_get, req.url)) {
+                next();
+            } else {
+                // res.render('auth_error');
+                next();
+            }*/
         } else {
-            // res.render('auth_error');
-            next();
+            res.status(403).send('u r blocked for sending too many request..');
+            if(!printed_black_list[req.ip]){
+                console.log("*** too many requests from -> " + req.ip);
+                printed_black_list[req.ip] = true ;
+            }
+            
         }
     };
 
@@ -40,14 +63,26 @@ module.exports = (function () {
 
 //        console.log("star");
 
-        db.contacts.findOne({_id: req.session.q }).lean().exec(
-            function (err, contact) {
-                if (contact) {
-                        req.contact = contact;
-                } else {
-                    next();
-                }
-            });
+        black_list[req.ip] = black_list[req.ip] + 1 || 1 ;
+
+        if( black_list[req.ip] < global.init.req_per_min){
+            next();
+            /*db.contacts.findOne({_id: req.session.q }).lean().exec(
+                function (err, contact) {
+                    if (contact) {
+                            req.contact = contact;
+                    } else {
+                        next();
+                    }
+                });*/
+        } else {
+            res.status(403).send('u r blocked for sending too many request...');
+            if(!printed_black_list[req.ip]){
+                console.log("*** too many requests from -> " + req.ip);
+                printed_black_list[req.ip] = true ;
+            }
+            
+        }  
     };
 
     return _return;
